6e74b1141b8854310e82467f1a00698d5489bfcb
Pavan Uthsara <uthsarapavan@gmail.com>
2025-08-28 06:51:57 +0530

    cashier and admin selector page v0.1

---
+++ frontend/src/pages/CashierLogIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CashierLogIn() {
+  return (
+    <div>CashierLogIn</div>
+  )
+}
+
+export default CashierLogIn
---
+++ frontend/src/pages/First.jsx
@@ -0,0 +1,15 @@
+
+function First() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-20 border-solid border-2 justify-self-auto">
+        <p>Cashier</p>
+      </div>
+      <div className="w-20">
+        <p>Admin</p>
+      </div>
+    </div>
+  );
+}
+
+export default First;
