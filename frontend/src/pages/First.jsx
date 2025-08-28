25e177007a8b69ed0dd72ac2e5c594c2d0f2ca48
Pavan Uthsara <uthsarapavan@gmail.com>
2025-08-28 07:02:25 +0530

    cashier and admin selector page v0.2.1

--- frontend/src/pages/First.jsx
+++ frontend/src/pages/First.jsx
@@ -2,14 +2,17 @@
 This page is to select whether user is admin or cashier.
 Then user will redirect to their login page.
 */
+import Reactpic from "../assets/react.svg"
 
 function First() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-20 border-solid border-2 justify-self-auto">
-        <p>Cashier</p>
+      <div className="w-52 border-solid border-2 justify-items-center p-10 m-4">
+        <img src={Reactpic} className="my-2"/>
+        <p className="">Cashier</p>
       </div>
-      <div className="w-20">
+      <div className="w-52 border-solid border-2 justify-items-center p-10 m-4">
+        <img src={Reactpic} className="my-2"/>
         <p>Admin</p>
       </div>
     </div>
